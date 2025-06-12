a5e9e3f4030794e19813c7dd3d030adf2a69e371
andreleao1 <andre.gustavo.ls@hotmail.com>
2025-06-12 11:30:51 -0300

    predict script improvements

--- predict.py
+++ predict.py
@@ -1,34 +1,19 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    #custom_trained_yolom = "../trained_models/yolo11m_oxford_tower_custom.pt"
-    custom_trained_yolon = "test_pruned.pt"
+    custom_trained_yolon = "./pruned/3_percent/pruned_yolo11n.pt"
 
     model = YOLO(custom_trained_yolon)
 
-    metrics = model.val(verbose=True)
-
-    accuracy = metrics.box.map * 100
-
     oxford_test_dataset = "./datasets/images/test"
-    oxford_tower_video = "./datasets/oxfordtower.mp4"
-    random_person_video = "./datasets/persons.mp4"
-    output_video = "runs/results/prediction_result.mp4"
 
-    # for i in range(5):
-    #     print(f"\nExecutando predicao {i+1} de 5...")
-    #     results = model.predict(
-    #         source=oxford_test_dataset,
-    #         save=True,
-    #         project="runs",
-    #         name=f"results_{i+1}",
-    #         save_txt=False,
-    #         show=True
-    #     )
-
-    print("\nMetricas finais:")
-    print("mAP50-95:", metrics.box.map)
-    print("mAP50:", metrics.box.map50)
-    print("mAP75:", metrics.box.map75)
-    print("mAP per category:", metrics.box.maps)
-    print(f"Percentual (mAP50-95): {accuracy:.2f}%")
+    for i in range(5):
+        print(f"\nExecutando predicao {i+1} de 5...")
+        results = model.predict(
+            source=oxford_test_dataset,
+            save=False,
+            project="runs",
+            name=f"results_{i+1}",
+            save_txt=False,
+            show=False
+        )
